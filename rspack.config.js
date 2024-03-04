4b9d10900ba7b661bbddf88221c635ef3ff34c0c
Douglas Fachel <dougfacha@gmail.com>
2024-03-04 09:00:48 -0300

    first commit

---
+++ rspack.config.js
@@ -0,0 +1,91 @@
+const rspack = require("@rspack/core");
+const refreshPlugin = require("@rspack/plugin-react-refresh");
+const isDev = process.env.NODE_ENV === "development";
+
+const {
+  AD_SERVICE_ADDR = '',
+  CART_SERVICE_ADDR = '',
+  CHECKOUT_SERVICE_ADDR = '',
+  CURRENCY_SERVICE_ADDR = '',
+  PRODUCT_CATALOG_SERVICE_ADDR = '',
+  RECOMMENDATION_SERVICE_ADDR = '',
+  SHIPPING_SERVICE_ADDR = '',
+  ENV_PLATFORM = '',
+  OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = '',
+  OTEL_SERVICE_NAME = 'frontend',
+  PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = '',
+} = process.env;
+
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+module.exports = {
+	context: __dirname,
+	entry: {
+		main: "./src/main.tsx"
+	},
+	resolve: {
+		extensions: ["...", ".ts", ".tsx", ".jsx"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svg$/,
+				type: "asset"
+			},
+			{
+				test: /\.(jsx?|tsx?)$/,
+				use: [
+					{
+						loader: "builtin:swc-loader",
+						options: {
+							sourceMap: true,
+							jsc: {
+								parser: {
+									syntax: "typescript",
+									tsx: true
+								},
+								transform: {
+									react: {
+										runtime: "automatic",
+										development: isDev,
+										refresh: isDev
+									}
+								}
+							},
+							env: {
+								targets: [
+									"chrome >= 87",
+									"edge >= 88",
+									"firefox >= 78",
+									"safari >= 14"
+								],
+								AD_SERVICE_ADDR,
+								CART_SERVICE_ADDR,
+								CHECKOUT_SERVICE_ADDR,
+								CURRENCY_SERVICE_ADDR,
+								PRODUCT_CATALOG_SERVICE_ADDR,
+								RECOMMENDATION_SERVICE_ADDR,
+								SHIPPING_SERVICE_ADDR,
+								OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
+								NEXT_PUBLIC_PLATFORM: ENV_PLATFORM,
+								NEXT_PUBLIC_OTEL_SERVICE_NAME: OTEL_SERVICE_NAME,
+								NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT: PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT,
+							}
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new rspack.DefinePlugin({
+			"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+		}),
+		new rspack.ProgressPlugin({}),
+		new rspack.HtmlRspackPlugin({
+			template: "./index.html"
+		}),
+		isDev ? new refreshPlugin() : null
+	].filter(Boolean)
+};
---
+++ src/main.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { Resource } from '@opentelemetry/resources';
+import './index.css';
+
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import {
+  BatchSpanProcessor,
+  TracerConfig,
+  WebTracerProvider,
+} from '@opentelemetry/sdk-trace-web';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+const collectorOptions = {
+  url: 'https://otlp.nr-data.net:443',
+  headers: {
+    'api-key': 'NRAK-9DZ50XGAZDTYPKKC4H8IS29TN20'
+  }
+};
+
+const providerConfig: TracerConfig = {
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'my-react-app',
+  }),
+};
+
+const provider = new WebTracerProvider(providerConfig);
+
+provider.addSpanProcessor(
+  new BatchSpanProcessor(new OTLPTraceExporter(collectorOptions)),
+);
+
+provider.register({
+  contextManager: new ZoneContextManager(),
+});
+
+registerInstrumentations({
+  instrumentations: [getWebAutoInstrumentations()],
+});
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
